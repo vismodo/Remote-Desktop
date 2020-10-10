5e88838e15ab9232e8c289fbacd2243e6dc074d2
VismayaAtreya <62926183+vismodo@users.noreply.github.com>
2020-10-10 15:18:23 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# Client.py of 'Remote Desktop'
+
+import pyautogui as pg
+import socket
+
+host = '192.168.0.17'  # as both code is running on same pc
+port = int(input('Port'))  # socket server port number
+
+client_socket = socket.socket()  # instantiate
+client_socket.connect((host, port))  # connect to the server
+
+message = 'done'
+while True:
+    try:
+        while message.lower().strip() != 'bye':
+            client_socket.send(message.encode())  # send message
+            data = client_socket.recv(1024).decode()  # receive response
+            if data == 'click':
+                pg.click(x, y)
+            elif data == 'del':
+                pg.typewrite(['backspace'])
+            elif data.startswith('cde:'):
+                pg.write(data.replace('cde:', ''))
+            elif data=='rclick':
+                pg.click(button='right')
+            elif data=='dclick':
+                pg.click(clicks=2)
+            elif data=='nl':
+                pg.typewrite(['enter'])
+            else:
+                x = int(data.split(' ')[0])
+                y = int(data.split(' ')[1])
+                pg.moveTo(x, y)  # show in terminal
+            message = 'done' # again take input
+
+        client_socket.close()  # close the connection
+    except:
+        pass
+# Created by vismodo: https://github.com/vismodo/
+# Email: vismaya.atreya@outlook.com
+# Repository: https://github.com/vismodo/Remote-Desktop (Remote Desktop)
+# Python Version: 3.9
